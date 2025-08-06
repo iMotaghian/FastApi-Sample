d82737c42190317e614b2983656fac6edad0c668
Mehrdad Motaghian <mehrdad.mot@gmail.com>
2025-08-06 16:43:12 +0330

    Update main_list.py

--- main_list.py
+++ main_list.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI,HTTPException,status
+from fastapi import FastAPI,HTTPException,status,Query,Path,Form,Body,File,UploadFile
+from fastapi.responses import JSONResponse
+from typing import Annotated,Optional,List
+from contextlib import asynccontextmanager
 import random
-app = FastAPI()
+
+@asynccontextmanager # @app.on_event("startup")
+async def lifespan(app: FastAPI):
+    print("Application startup")
+    yield 
+    print("Application shutdown")
+
+app = FastAPI(lifespan=lifespan)
 
 names_list = [
     {"id":1, "name":"ali"},
@@ -10,35 +20,52 @@ names_list = [
     {"id":5, "name":"zahra"},
 ]
 
-@app.get("/")
-def name_list():
+
+@app.get("/") # query
+def name_list(q : str = Query(alias="search",description="can search",example="ali",default=None,max_length=50)): # def name_list(q : Annotated[str, Query(max_length=50)] = None ): # def name_list(q : Optional[str] = None) # def name_list(q : str | None = None)
+    if q:
+        return [ item for item in names_list if item["name"] == q ]
     return names_list
 
-@app.get("/{id}")
-def name(id:int):
+@app.get("/{id}") # path
+def name(id:int = Path(alias="id alias",title="id",description="inja path ast")):
     for name in names_list:
         if name["id"] == id:
             return name
-    return HTTPException(detail={"not found"},status_code=status.HTTP_404_NOT_FOUND)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="not found")
 
-@app.post("/")
-def add_name(name:str):
+@app.post("/",status_code=status.HTTP_201_CREATED) # body
+def add_name(name:str = Body(embed=True)):
     temp = {"id":random.randint(6,100),"name":name}
     names_list.append(temp)
     return names_list
 
-@app.put("/{id}")
-def name_update(id:int,nname:str):
+@app.put("/{id}",status_code=status.HTTP_200_OK) # form
+def name_update(id:int = Path(),nname:str = Form(description="in form ast",default="mehrdad")):
     for name in names_list:
         if name["id"] == id:
             name["name"] = nname
             return name
-    return HTTPException(detail={"not found"},status_code=status.HTTP_404_NOT_FOUND)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="not found")
 
-@app.delete("/")
+@app.delete("/{id}")
 def delete_name(id:int):
     for name in names_list:
         if name["id"] == id:
             names_list.remove(name) 
-            return "deleted"
-    return HTTPException(detail={"not found"},status_code=status.HTTP_404_NOT_FOUND)
+            return JSONResponse(content={"details":"deleted"},status_code=status.HTTP_200_OK)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="not found")
+
+
+
+@app.post("/upload_file/")
+async def upload_file(file: UploadFile = File(...)):
+    content = await file.read() # Asynchronous reading
+    return {"filename": file.filename, "content_type": file.content_type, "file_size": len(content)}
+
+@app.post("/upload-multiple/")
+async def upload_multiple(files: List[UploadFile]):
+    return [
+        {"filename": file.filename, "content_type": file.content_type}
+        for file in files
+    ]
